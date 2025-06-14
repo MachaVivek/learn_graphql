23b2a217e25ac9c14dc6ab397fe9846e166fec3f
Vivek Macha <machavivek@eksaq.in>
2025-06-14 15:20:09 +0530

    organised the code into folders and implemented authentication authorization

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,36 @@
 import express from 'express'
-import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { prismaClient } from './lib/db';
+import createApolloGraphqlServer from './graphql';
+import UserService from './services/user';
+import dotenv from 'dotenv';
+dotenv.config();
 
 async function init(){
     const app= express();
     const port = Number(process.env.PORT) || 8000;
 
     app.use(express.json())  // it parses incomming requests into JSON body
-    const graphql_server= new ApolloServer({
-        typeDefs: `
-            type Query {
-                hello: String
-            }
-            type Mutation{
-                createUser(firstName: String!, lastName: String!, email: String!, password: String!) : Boolean
-            }
-            `,   // we write the schema for graphql
-            
-        resolvers: {
-            Query:{
-                hello: () => `Working grahpql`
-            },
-            Mutation:{
-                createUser: async (_, {firstName, lastName, email, password}:{firstName: string, lastName:string, email:string, password: string})=>{
-                    {
-                        await prismaClient.users.create({
-                            data:{
-                                firstName,
-                                lastName,
-                                email,
-                                password,
-                                salt: 'random_salt'
-                            }
-                        })
-                        return true
-                    };
-                }
-            }
-        }  // actual function which are executed
-    })
-    await graphql_server.start();
-
 
     app.get("/", (req, res) => {
         res.status(200).json({message:"Backend is working"})
     })
 
-    app.use('/graphql', expressMiddleware(graphql_server)) // here apply the middleware and 
+    const graphql_server= await createApolloGraphqlServer()
+    app.use('/graphql',
+        express.json(), // <- parses incoming requests
+        expressMiddleware(graphql_server, {
+          context: async ({ req }) => {
+            const token = req.headers["token"] || 'Token not present';
+            try {
+              const user = UserService.decodeJwtToken(token as string);
+              return { user };
+            } catch (error) {
+              return {};
+            }
+          },
+        })
+      ); // here apply the middleware 
+
     app.listen(port, ()=>{
         console.log(`Server started at port: ${port}`)
     })
